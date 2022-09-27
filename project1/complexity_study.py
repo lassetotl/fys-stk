4a9fe4fa1017e14a4761a3f4f6343229c52ff4c9
momentomori99 <56488501+momentomori99@users.noreply.github.com>
2022-09-27 20:00:05 +0200

    Måtte bare oppdatere noe

--- project1/complexity_study.py
+++ project1/complexity_study.py
@@ -1,57 +1,3 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from sklearn.utils import resample
-
-
-def MSE(y_data,y_model): #mean square error
-    n = np.size(y_model)
-    return np.sum((y_data-y_model)**2)/n
-
-def FrankeFunction(x,y):
-    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-    return term1 + term2 + term3 + term4
-
-#from lecture notes on Ridge/Lasso regression
-def create_X(x, y, n): #design matrices for polynomials up to 10th degree
-    if len(x.shape) > 1:
-        x = np.ravel(x)
-        y = np.ravel(y)
-
-    N = len(x)
-    l = int((n+1)*(n+2)/2) # Number of elements in beta
-    X = np.ones((N,l))
-
-    for i in range(1,n+1):
-        q = int((i)*(i+1)/2)
-        for k in range(i+1):
-            X[:,q+k] = (x**(i-k))*(y**k)
-    return X
-
-# Making meshgrid of datapoints and compute Franke's function
-n = 5 #in task b; up to 5 (this affects the approximate terrain plot)
-N = 100
-x = np.sort(np.random.uniform(0, 1, N))
-y = np.sort(np.random.uniform(0, 1, N))
-
-x_, y_ = np.meshgrid(x,y)
-
-#how does the scaling affect the experiment?
-var = 0.1 #variance of noise (?)
-noise = np.random.normal(0, var, len(x_)*len(x_))
-noise = noise.reshape(len(x_), len(x_))
-
-z = FrankeFunction(x_, y_) + noise #Franke with noise
-
-#######################################################################
-
-
 def model_complexity(degrees, trial):
     """
     It is assumed that you have defined func. for MSE(y_data, y_model) globaly.
@@ -62,6 +8,12 @@ def model_complexity(degrees, trial):
         degrees: maximum degree range for complexity calc. Recommended: 30
         trial: number of resamples. Recommended: 100
 
+
+    Output:
+        3 arrays, testerror, trainerror and polynomial.
+        testerror and trainerror contain mean values of MSE of trial number of repeated resamples.
+
+        Additionaly, it plots teserror, trainerror against polynomial number.
     """
     import os
     import numpy as np
@@ -119,4 +71,3 @@ def model_complexity(degrees, trial):
     plt.legend()
     plt.savefig("Complexity_study.png")
     plt.show()
-model_complexity(35, 30)
