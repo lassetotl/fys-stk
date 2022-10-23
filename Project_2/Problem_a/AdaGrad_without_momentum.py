7232e97ec6e9b9bc2bb1a3295432e3e0af061b25
momentomori99 <56488501+momentomori99@users.noreply.github.com>
2022-10-23 18:45:48 +0200

    Small changes and added AdeGrad method
    very nice.

---
+++ Project_2/Problem_a/AdaGrad_without_momentum.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from autograd import grad
+
+
+#Generating data:
+np.random.seed(10)
+n = 100
+x = 2 * np.random.randn(n,1)
+y = -0.5*x**3 + 1.5*x**2 + 1.5*x + 3*np.random.randn(n,1)
+
+X = np.c_[np.ones((n, 1)), x, x**2, x**3]
+H = (2.0/n) * X.T @ X
+EigVal, EigVec = np.linalg.eig(H)
+
+def gradient(X, y, Beta, n):
+    return (2.0/n) * X.T @ (X @ Beta - y)
+
+#learn_rate = 1.0/np.max(EigVal)
+n_epochs = 50
+M = 5
+m = int(n/M) #number of minibatches
+Beta = np.random.randn(4,1)
+learn_rate = 0.01
+epsilon  = 1e-8
+for epoch in range(n_epochs):
+    G = np.zeros(shape=(np.shape(X)[1],np.shape(X)[1]))
+    G = np.zeros(shape=(4,4))
+    for i in range(m):
+        random_index = M*np.random.randint(m)
+        X_minibatch = X[random_index:random_index + M]
+        y_minibatch = y[random_index:random_index + M]
+        g = (1/M) * gradient(X_minibatch, y_minibatch, Beta, n)
+        G = G + (g @ g.T)
+        G_inverse = np.c_[learn_rate / ( epsilon + np.sqrt(np.diagonal(G)) )]
+        update = np.multiply(G_inverse, g)
+        Beta = Beta - update
+print(Beta)
+# test_x = np.linspace(-4, 4, n)
+# test_X = np.c_[np.ones((n, 1)), test_x, test_x**2, test_x**3]
+y_predict = X @ Beta
+
+plt.plot(x, y, "o", label="real data")
+plt.plot(x, y_predict, "o", label="Prediction")
+plt.legend()
+plt.show()
