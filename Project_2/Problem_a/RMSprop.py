08a515a6a9703377f2259d494cc6940d4a13c28b
momentomori99 <56488501+momentomori99@users.noreply.github.com>
2022-10-24 19:10:20 +0200

    Finishing up problem a

--- Project_2/Problem_a/RMSprop.py
+++ Project_2/Problem_a/RMSprop.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from autograd import grad
 
 
 #Generating data:
@@ -16,16 +15,16 @@ EigVal, EigVec = np.linalg.eig(H)
 def gradient(X, y, Beta, n):
     return (2.0/n) * X.T @ (X @ Beta - y)
 
-#learn_rate = 1.0/np.max(EigVal)
+
 n_epochs = 50
 M = 5
-m = int(n/M) #number of minibatches
 Beta = np.random.randn(4,1)
 learn_rate = 0.01
+m = int(n/M) #number of minibatches
 epsilon  = 1e-8
 
-change = 0.0
-momentum = 0.3
+rho = 0.99
+
 
 for epoch in range(n_epochs):
     G = np.zeros(shape=(np.shape(X)[1],np.shape(X)[1]))
@@ -34,17 +33,14 @@ for epoch in range(n_epochs):
         X_minibatch = X[random_index:random_index + M]
         y_minibatch = y[random_index:random_index + M]
         g = (1/M) * gradient(X_minibatch, y_minibatch, Beta, n)
-        G = G + (g @ g.T)
-        G_inverse = np.c_[learn_rate / ( epsilon + np.sqrt(np.diagonal(G)) )]
-        new_change = np.multiply(G_inverse, g) + momentum*change
-        change = new_change
-        Beta = Beta - new_change
-print(Beta)
-# test_x = np.linspace(-4, 4, n)
-# test_X = np.c_[np.ones((n, 1)), test_x, test_x**2, test_x**3]
-y_predict = X @ Beta
 
-plt.plot(x, y, "o", label="real data")
-plt.plot(x, y_predict, "o", label="Prediction")
-plt.legend()
-plt.show()
+        prev_G = G
+        G = G + (g @ g.T)
+
+        G_new = (rho * prev_G + (1 - rho) * G)
+
+        G_inverse = np.c_[learn_rate / (epsilon + np.sqrt(np.diagonal(G_new)))]
+        update = np.multiply(G_inverse , g)
+        Beta = Beta - update
+
+print(Beta)
