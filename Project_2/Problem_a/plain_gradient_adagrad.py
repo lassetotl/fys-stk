26e9aea99606fa93e9c953fdf9d7f7d5799fa28c
momentomori99 <56488501+momentomori99@users.noreply.github.com>
2022-11-07 13:51:12 +0100

    ..

--- Project_2/Problem_a/plain_gradient_adagrad.py
+++ Project_2/Problem_a/plain_gradient_adagrad.py
@@ -9,6 +9,18 @@ def plain_gradient_adagrad(learn_rate, n_iterations, X, y, Beta):
         g = gradient(n, X, y, Beta)
         G = G + (g @ g.T)
         G_inverse = np.c_[learn_rate / ( epsilon + np.sqrt(np.diagonal(G)) )]
+        learn_rate = G_inverse
         update = np.multiply(G_inverse, g)
         Beta = Beta - update
     return Beta
+
+
+# def plain_gradient_adagrad_new(learn_rate, n_iterations, X, y, Beta):
+#     n = np.shape(X)[0]
+#     epsilon  = 1e-13
+#     for i in range(n_iterations):
+#         g = gradient(n, X, y, Beta)
+#         for j in range(i):
+#             G = g @ g.T
+#
+#     return Beta
