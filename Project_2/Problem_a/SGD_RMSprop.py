fede53da48f25b7b1225b93f7661d6c58ffd81fd
momentomori99 <56488501+momentomori99@users.noreply.github.com>
2022-11-08 14:08:10 +0100

    All code and analyse for a) is finished
    Kunne tweake litt på parameterene, litt rart at ADAM ikke er det beste GD

---
+++ Project_2/Problem_a/SGD_RMSprop.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from gradient import *
+
+
+def SGD_RMSprop(M, n_epochs, learn_rate, X, y, Beta):
+    n = np.shape(X)[0]
+    m = int(n/M) #number of minibatches
+    epsilon  = 1e-8
+    rho = 0.99
+
+    for epoch in range(n_epochs):
+        G = np.zeros(shape=(np.shape(X)[1],np.shape(X)[1]))
+        for i in range(m):
+            random_index = M*np.random.randint(m)
+            X_minibatch = X[random_index:random_index + M]
+            y_minibatch = y[random_index:random_index + M]
+            g = (1/M) * gradient(n, X_minibatch, y_minibatch, Beta)
+            prev_G = G
+            G = G + (g @ g.T)
+            G_new = (rho * prev_G + (1 - rho) * G)
+            G_inverse = np.c_[learn_rate / (epsilon + np.sqrt(np.diagonal(G_new)))]
+            update = np.multiply(G_inverse , g)
+            Beta = Beta - update
+    return Beta
--- Project_2/Problem_a/analyse.py
+++ Project_2/Problem_a/analyse.py
@@ -9,6 +9,8 @@ from plain_gradient_adagrad import *
 from plain_gradient_adagrad_momentum import *
 from SGD_adagrad import *
 from SGD_adagrad_momentum import *
+from SGD_RMSprop import *
+from SGD_ADAM import *
 
 def MSE(y_data,y_model):
     n = np.size(y_model)
@@ -22,76 +24,100 @@ x, y, X, max_EigVal = DataPoints(n).output()
 learn_rate = 1/max_EigVal
 n_iterations = 70
 Beta_start = np.random.randn(np.shape(X)[1],1)
-M = 5 #Size of each minibatch
+M = 10 #Size of each minibatch
 n_epoch = 40 #number of epochs
 
 
 """
 Analysis of plain gradient descents:
 """
-N = np.zeros(n_iterations)
-MSE1 = np.zeros(n_iterations)
-MSE2 = np.zeros(n_iterations)
-MSE3 = np.zeros(n_iterations)
-MSE4 = np.zeros(n_iterations)
-for i in range(n_iterations):
-    Beta1 = plain_gradient(learn_rate, i, X, y, Beta_start )
-    y_pred1 = X @ Beta1
-    MSE1[i] = MSE(y,y_pred1)
-    Beta2 = plain_gradient_momentum(learn_rate, i, X, y, Beta_start)
-    y_pred2 = X @ Beta2
-    MSE2[i] = MSE(y,y_pred2)
-    Beta3 = plain_gradient_adagrad(learn_rate, i, X, y, Beta_start)
-    y_pred3 = X @ Beta3
-    MSE3[i] = MSE(y,y_pred3)
-    Beta4 = plain_gradient_adagrad_momentum(learn_rate, i, X, y, Beta_start)
-    y_pred4 = X @ Beta4
-    MSE4[i] = MSE(y,y_pred4)
+def plain_GD_analysis():
+    N = np.zeros(n_iterations)
+    MSE1 = np.zeros(n_iterations)
+    MSE2 = np.zeros(n_iterations)
+    MSE3 = np.zeros(n_iterations)
+    MSE4 = np.zeros(n_iterations)
+    for i in range(n_iterations):
+        Beta1 = plain_gradient(learn_rate, i, X, y, Beta_start )
+        y_pred1 = X @ Beta1
+        MSE1[i] = MSE(y,y_pred1)
+        Beta2 = plain_gradient_momentum(learn_rate, i, X, y, Beta_start)
+        y_pred2 = X @ Beta2
+        MSE2[i] = MSE(y,y_pred2)
+        Beta3 = plain_gradient_adagrad(learn_rate, i, X, y, Beta_start)
+        y_pred3 = X @ Beta3
+        MSE3[i] = MSE(y,y_pred3)
+        Beta4 = plain_gradient_adagrad_momentum(learn_rate, i, X, y, Beta_start)
+        y_pred4 = X @ Beta4
+        MSE4[i] = MSE(y,y_pred4)
 
 
 
 
-    N[i] = i
+        N[i] = i
 
-plt.plot(N, MSE1, label="plain gradient")
-plt.plot(N, MSE2, label="plain gradient with momentum")
-plt.plot(N, MSE3, label="plain gradient with adagrad")
-plt.plot(N, MSE4, "--r", label="plain gradient with adagrad and momentum")
-plt.legend()
-plt.show()
+    plt.plot(N, MSE1, label="plain gradient")
+    plt.plot(N, MSE2, label="plain gradient with momentum")
+    plt.plot(N, MSE3, label="plain gradient with adagrad")
+    plt.plot(N, MSE4, "--r", label="plain gradient with adagrad and momentum")
+    plt.legend()
+    plt.show()
 """
 Analysis of stochastic gradient descents
 """
-learn_rate = 0.01
-N = np.zeros(n_epoch)
-MSE1 = np.zeros(n_epoch)
-MSE2 = np.zeros(n_epoch)
-MSE3 = np.zeros(n_epoch)
-MSE4 = np.zeros(n_epoch)
+def SGD_analyse():
+    learn_rate = 0.01
+    N = np.zeros(n_epoch)
+    MSE1 = np.zeros(n_epoch)
+    MSE2 = np.zeros(n_epoch)
+    MSE3 = np.zeros(n_epoch)
+    MSE4 = np.zeros(n_epoch)
+    MSE5 = np.zeros(n_epoch)
+    MSE6 = np.zeros(n_epoch)
 
-for i in range(n_epoch):
-    Beta1 = SGD(M, i, learn_rate, X, y, Beta_start)
-    y_pred1 = X @ Beta1
-    MSE1[i] = MSE(y,y_pred1)
+    for i in range(n_epoch):
+        Beta1 = SGD(M, i, learn_rate, X, y, Beta_start)
+        y_pred1 = X @ Beta1
+        MSE1[i] = MSE(y,y_pred1)
 
-    Beta2 = SGD_momentum(M, i, learn_rate, X, y, Beta_start)
-    y_pred2 = X @ Beta2
-    MSE2[i] = MSE(y,y_pred2)
+        Beta2 = SGD_momentum(M, i, learn_rate, X, y, Beta_start)
+        y_pred2 = X @ Beta2
+        MSE2[i] = MSE(y,y_pred2)
 
-    Beta3 = SGD_adagrad(M, i, learn_rate, X, y, Beta_start)
-    y_pred3 = X @ Beta3
-    MSE3[i] = MSE(y,y_pred3)
+        Beta3 = SGD_adagrad(M, i, learn_rate, X, y, Beta_start)
+        y_pred3 = X @ Beta3
+        MSE3[i] = MSE(y,y_pred3)
 
-    Beta4 = SGD_adagrad_momentum(M, i, learn_rate, X, y, Beta_start)
-    y_pred4 = X @ Beta4
-    MSE4[i] = MSE(y,y_pred4)
+        Beta4 = SGD_adagrad_momentum(M, i, learn_rate, X, y, Beta_start)
+        y_pred4 = X @ Beta4
+        MSE4[i] = MSE(y,y_pred4)
+
+        Beta5 = SGD_RMSprop(M, i, learn_rate, X, y, Beta_start)
+        y_pred5 = X @ Beta5
+        MSE5[i] = MSE(y,y_pred5)
+
+        Beta6 = SGD_ADAM(M, i, learn_rate, X, y, Beta_start)
+        y_pred6 = X @ Beta6
+        MSE6[i] = MSE(y,y_pred6)
 
 
-    N[i] = i
+        N[i] = i
 
-plt.plot(N, MSE1, label="simple SDG")
-plt.plot(N, MSE2, label="simple SDG with momentum")
-plt.plot(N, MSE3, label="simple SDG with adagrad")
-plt.plot(N, MSE4, label="simple SDG with adagrad and momentum")
-plt.legend()
-plt.show()
+    plt.plot(N, MSE1, label="simple SDG")
+    plt.plot(N, MSE2, label="simple SDG with momentum")
+    plt.plot(N, MSE3, label="simple SDG with adagrad")
+    plt.plot(N, MSE4, label="simple SDG with adagrad and momentum")
+    plt.plot(N, MSE5, label="simple SDG with RMSProp method")
+    plt.plot(N, MSE6, label="simple SDG with ADAM method")
+    plt.grid(1)
+    plt.legend()
+    plt.show()
+#
+# M = 5
+# SGD_analyse()
+# M = 10
+# SGD_analyse()
+# M = 20
+# SGD_analyse()
+# learn_rate = 0.1
+# SGD_analyse()
